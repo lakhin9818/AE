4f311122cf8e179a0b2156a715bc422c0726ea9b
lakhin9818 <lakhingupta@gmail.com>
2025-03-22 19:58:51 +0530

    New TC

--- src/main/java/Base/Listeners.java
+++ src/main/java/Base/Listeners.java
@@ -1,11 +1,24 @@
 package Base;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 //import org.testng.TestListenerAdapter;
 
 public class Listeners implements ITestListener {
+//	WebDriver driver;
+
+//
 	@Override
 	public void onFinish(ITestContext contextFinish) {
 		System.out.println("onFinish method finished");
@@ -25,8 +38,24 @@ public class Listeners implements ITestListener {
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		System.out.println("Method failed" + result.getName());
+		Object testClass = result.getInstance();
+		if (testClass instanceof TestBase) {
+			WebDriver driver = ((TestBase) testClass).getDriver();
 
+			if (driver != null) {
+				System.out.println("✅ WebDriver is initialized. Capturing screenshot...");
+
+				try {
+					takeSnapShot(driver, result.getName());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else {
+				System.out.println("🚨 WebDriver is NULL, cannot take screenshot.");
+			}
+		}
 	}
 
 	@Override
@@ -44,4 +73,42 @@ public class Listeners implements ITestListener {
 		System.out.println("Method passed" + result.getName());
 
 	}
+
+	public static void takeSnapShot(WebDriver driver, String testName) throws Exception {
+//		// Convert web driver object to TakeScreenshot
+//		String ss = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//
+//		String nameWithTime = ss + ".png";
+////		TakesScreenshot scrShot = ((TakesScreenshot) driver);
+//		// Call getScreenshotAs method to create image file
+////		File SrcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//		String SrcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+//		File svd = new File(SrcFile);
+//		String screenshotPath = ".Screenshots" + testName + "_" + nameWithTime;
+//
+//		// Move image file to new destination
+//		File DestFile = new File(screenshotPath);
+//		// Copy file at destination
+//		FileUtils.copyFile(svd, DestFile);
+		try {
+			File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+			if (srcFile.exists()) { // ✅ Ensure the file exists before copying
+				String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+				String screenshotPath = "D:\\lakhin\\AE\\Screenshots\\" + testName + "_" + timestamp + ".png";
+
+				File destFile = new File(screenshotPath);
+				FileUtils.copyFile(srcFile, destFile);
+				System.out.println("✅ Screenshot saved at: " + screenshotPath);
+			} else {
+				System.out.println("🚨 Screenshot file does not exist! Skipping copy.");
+			}
+
+		} catch (IOException e) {
+			System.out.println("🚨 Error while saving screenshot: " + e.getMessage());
+		} catch (WebDriverException e) {
+			System.out.println("🚨 WebDriverException while taking screenshot: " + e.getMessage());
+		}
+	}
+
 }
---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,56 @@
+package Pages;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import Base.JavaScriptOperation;
+import Base.PropertyReader;
+import Base.WaitHelper;
+
+public class LoginPage {
+	WebDriver driver;
+	WaitHelper wait;
+	JavaScriptOperation js;
+	PropertyReader prop = new PropertyReader();
+
+	@FindBy(xpath = "//a[contains(text(),'Sign')]")
+	private WebElement SignUpButton;
+	@FindBy(xpath = "//input[@data-qa=\"login-email\"]")
+	private WebElement UserEmail;
+	@FindBy(xpath = "//input[@data-qa=\"login-password\"]")
+	private WebElement LoginPassword;
+	@FindBy(xpath = "//button[@data-qa=\"login-button\"]")
+	private WebElement LoginButton;
+	@FindBy(xpath = "//h2[text()=\"Login to your account\"]")
+	private WebElement LogintabMessage;
+	@FindBy(xpath = "//li//a[contains(text(),'Logged')]")
+	private WebElement loggedUserMEssage;
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		this.wait = new WaitHelper(driver);
+		this.js = new JavaScriptOperation(driver);
+	}
+
+	public void loginUser() {
+		SignUpButton.click();
+		wait.waitForElementToBeVisible(LogintabMessage, 20);
+		assertTrue(LogintabMessage.isDisplayed());
+		UserEmail.sendKeys(prop.getProperty("NewUserEmail"));
+		LoginPassword.sendKeys(prop.getProperty("NewUserPassword"));
+		LoginButton.click();
+	}
+
+	public void ValidaLoggedUser() {
+		String Actual = loggedUserMEssage.getText() + "828";
+		String Expected = "Logged in as" + " " + prop.getProperty("NewUserName");
+		Assert.assertEquals(Actual, Expected);
+	}
+
+}
